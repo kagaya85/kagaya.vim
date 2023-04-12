c9b8fa0d8bfd897166b7a1101362f85d457e1752
Kagaya <kagaya85@outlook.com>
2023-04-12 13:44:03 +0800

    update to neovim 0.9

--- lua/core/options.lua
+++ lua/core/options.lua
@@ -55,7 +55,7 @@ local function load_options()
 		previewheight = 12,
 		pumheight = 15,
 		redrawtime = 1500,
-		relativenumber = true,
+		relativenumber = false,
 		ruler = true,
 		scrolloff = 2,
 		sessionoptions = "curdir,help,tabpages,winsize",
--- lua/modules/configs/completion/copilot-cmp.lua
+++ lua/modules/configs/completion/copilot-cmp.lua
@@ -1,9 +1,3 @@
 return function()
-	require("copilot_cmp").setup({
-		formatters = {
-			label = require("copilot_cmp.format").format_label_text,
-			insert_text = require("copilot_cmp.format").format_insert_text,
-			preview = require("copilot_cmp.format").deindent,
-		},
-	})
+	require("copilot_cmp").setup({})
 end
