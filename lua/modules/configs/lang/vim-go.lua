3d76313fd2560e9411dfb26b7eb6839a5dc37f9d
Kagaya <kagaya85@outlook.com>
2023-11-09 11:14:56 +0800

    update config

--- lua/modules/configs/lang/vim-go.lua
+++ lua/modules/configs/lang/vim-go.lua
@@ -2,5 +2,5 @@ return function()
 	vim.g.go_doc_keywordprg_enabled = 0
 	vim.g.go_def_mapping_enabled = 0
 	vim.g.go_code_completion_enabled = 0
-	vim.g.go_auto_type_info = 1
+	vim.g.go_auto_type_info = 0
 end
